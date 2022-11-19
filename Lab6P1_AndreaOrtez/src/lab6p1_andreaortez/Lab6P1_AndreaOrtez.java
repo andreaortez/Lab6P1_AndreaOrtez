15b0ec6971ff47434634db863640c0e2ddefda68
Andrea Ortez <andreaortez11@unitec.edu>
2022-11-18 19:21:09 -0600

    Lab Completado

--- Lab6P1_AndreaOrtez/src/lab6p1_andreaortez/Lab6P1_AndreaOrtez.java
+++ Lab6P1_AndreaOrtez/src/lab6p1_andreaortez/Lab6P1_AndreaOrtez.java
@@ -5,109 +5,98 @@ import java.util.Scanner;
 
 public class Lab6P1_AndreaOrtez {
 
-    static Scanner leer = new Scanner (System.in);
+    static Scanner leer = new Scanner(System.in);
     static SecureRandom random = new SecureRandom();
-    
+
     public static void main(String[] args) {
-        
+
         int opcion;
-        do{
+        do {
             System.out.println("-- MENU --");
             System.out.println("1-> Turing");
             System.out.println("2-> Constante de Kaprekar");
             System.out.println("3-> Salir");
             System.out.print("Ingrese una opcion: ");
             opcion = leer.nextInt();
-            
-            switch(opcion){
+
+            switch (opcion) {
                 case 1:
                     System.out.println("\n -- TURING EN PROCESO --");
                     System.out.print("Ingrese el tamaño de arreglo: ");
                     int tam = leer.nextInt();
-                    
-                    while (tam<0){
+
+                    while (tam < 0) {
                         System.out.print("Ingrese el tamaño de arreglo: ");
                         tam = leer.nextInt();
                     }
-                    int [] array = Lectura (tam);
-                    
+                    int[] array = Lectura(tam);
+
                     System.out.print("Arreglo generado: ");
-                    Imprimir (array);
+                    Imprimir(array);
                     System.out.print("\nIngrese cadena de instrucciones: ");
                     String ins = leer.next().toUpperCase();
-                    
-                    System.out.println("Cadena generada: " + Turing (ins,array) + "\n");
+
+                    System.out.println("Cadena generada: " + Turing(ins, array) + "\n");
                     break;
                 case 2:
                     System.out.println("\n-- CONSTANTE DE KAPREKAR EN PROCESO --");
-                    
-                    int var=0, cont=0;
-                    do{
+
+                    int cont = 0,
+                     num;
+                    boolean valid;
+
+                    do {
                         System.out.print("Ingrese un numero de 4 cifras: ");
-                        int num =leer.nextInt();
-                    
-                        while (num<1000 || num>9999){
-                            System.out.print("Ingrese un numero de 4 cifras: ");
-                            num =leer.nextInt();
-                        }
-                        
-                        if (num>=1000 || num<=9999){
-                            int cont2=0;
-                            String cad= num + "";
-                            for (int i=0; i<4; i++){
-                                char a = cad.charAt(i);
-                                for(int j=i+1; j<4; j++){
-                                    char b = cad.charAt(j);
-                                    if (a==b){
-                                        cont2++;
-                                    }
-                                }
-                                if (cont2==4){
-                                    System.out.println("Los numeros no pueden ser iguales");
-                                
-                                }else{
-                                    System.out.print(ConversionNum(Descendente(ConversionArray(num))));
-                                    System.out.print(" - ");
-                                    System.out.print(ConversionNum(Ascendente(ConversionArray(num)))); 
-                                    System.out.print(" = ");
-                                    System.out.println(Resta(Descendente(ConversionArray(num)),Ascendente(ConversionArray(num))));
-                                    var = Resta(Descendente(ConversionArray(num)),Ascendente(ConversionArray(num)));
-                                    System.out.println();
-                                    cont++;
-                                }    
+                        num = leer.nextInt();
+                        valid = true;
+
+                        if (num < 1000 || num > 9999) {
+                            valid = false;
+                        } else {
+                            int[] var2 = ConversionArray(num);
+                            if (var2[0] == var2[1] && var2[0] == var2[2] && var2[0] == var2[3]) {
+                                valid = false;
                             }
-                             
                         }
-                    
-                        
-                    }while (var!=6174 && cont<8);
-                    
+                    } while (!valid);
+
+                    do {
+                        System.out.print(ConversionNum(Descendente(ConversionArray(num))));
+                        System.out.print(" - ");
+                        System.out.print(ConversionNum(Ascendente(ConversionArray(num))));
+                        System.out.print(" = ");
+                        System.out.println(Resta(Descendente(ConversionArray(num)), Ascendente(ConversionArray(num))));
+                        num = Resta(Descendente(ConversionArray(num)), Ascendente(ConversionArray(num)));
+                        cont++;
+                    } while (num != 6174 && cont < 8);
+                    System.out.println();
             }
-        }while (opcion!=3);
+        } while (opcion
+                != 3);
     }
-    
-    public static int [] Lectura (int tam){
-        int [] temp = new int [tam];
-        for (int i=0; i<tam; i++){
-            temp[i]=random.nextInt(9)+0;
+
+    public static int[] Lectura(int tam) {
+        int[] temp = new int[tam];
+        for (int i = 0; i < tam; i++) {
+            temp[i] = random.nextInt(9) + 0;
         }
         return temp;
     }//Fin Lectura
-    
-    public static void Imprimir(int [] array){
-       for (int i=0; i<array.length; i++){
-           System.out.print("[" + array[i] + "]");
-       }
-   }//Fin imprimir
-    
-    public static String Turing (String ins, int [] array){
-        int apunt=0;
-        String cad="";
-        
-        while (apunt<array.length){
-            for(int i=0; i<ins.length(); i++){
+
+    public static void Imprimir(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("[" + array[i] + "]");
+        }
+    }//Fin imprimir
+
+    public static String Turing(String ins, int[] array) {
+        int apunt = 0;
+        String cad = "";
+
+        while (apunt < array.length) {
+            for (int i = 0; i < ins.length(); i++) {
                 char a = ins.charAt(i);
-                switch (a){
+                switch (a) {
                     case 'R':
                         apunt++;
                         break;
@@ -115,75 +104,76 @@ public class Lab6P1_AndreaOrtez {
                         apunt--;
                         break;
                     case 'X':
-                        if (apunt>=0){
-                            cad+=array[apunt];
+                        if (apunt >= 0) {
+                            cad += array[apunt];
                         }
                         break;
                     default:
                         break;
                 }
             }
-          apunt=array.length;
+            apunt = array.length;
         }
         return cad;
     }//Fin Turing
-    
-    public static int [] ConversionArray (int num){
+
+    public static int[] ConversionArray(int num) {
         String cad = num + "";
-        int [] array = new int [4];
-        for(int i=0; i<array.length; i++){
+        int[] array = new int[4];
+        for (int i = 0; i < array.length; i++) {
             char a = cad.charAt(i);
-            array[i]=a-48;
+            array[i] = a - 48;
         }
         return array;
     }
-    
-    public static String ConversionNum (int [] num){
-        String numf="";
-        for (int i=0; i<num.length; i++){
-           numf+=num[i];
-       }
+
+    public static String ConversionNum(int[] num) {
+        String numf = "";
+        for (int i = 0; i < num.length; i++) {
+            numf += num[i];
+        }
         return numf;
     }
-    public static int [] Ascendente (int [] array){
-        int [] asc = new int [4];
-        for (int i=0; i<4; i++){
-            int menor=array[i];//le damos un valor a menor para compararlo
-            for (int j=i+1; j<4; j++){
-                int b=array[j];//le damos el siguiente valor del arreglo a b para empezar a comparar
-                if (menor>=b){
-                    menor=b;//Si encuentra un valor menor al original, se sustituye para el nuevo valor
-                    array[j]=array[i];//se sustituye la posición de j y colocamos i para no perder el valor de i
-                    array[i]=menor;//se sustituye el valor de i al valor del menor para que ya no vuelvan a leer ese valor
+
+    public static int[] Ascendente(int[] array) {
+        int[] asc = new int[4];
+        for (int i = 0; i < 4; i++) {
+            int menor = array[i];//le damos un valor a menor para compararlo
+            for (int j = i + 1; j < 4; j++) {
+                int b = array[j];//le damos el siguiente valor del arreglo a b para empezar a comparar
+                if (menor >= b) {
+                    menor = b;//Si encuentra un valor menor al original, se sustituye para conservar el nuevo valor
+                    array[j] = array[i];//se sustituye la posición de j y colocamos i para no perder el valor de i
+                    array[i] = menor;//se sustituye el valor de i al valor del menor para que ya no vuelvan a leer ese valor
                 }
             }
-            asc[i]=menor;//asignamos el valor menor a la posicion en el arreglo
+            asc[i] = menor;//asignamos el valor menor a la posicion en el arreglo
         }
         return asc;
     }
-    
-    public static int [] Descendente (int [] array){
-        int [] asc = new int [4];
-        for (int i=0; i<4; i++){
-            int mayor=array[i];
-            for (int j=i+1; j<4; j++){
-                int b=array[j];
-                if (mayor<=b){
-                    mayor=b;
-                    array[j]=array[i];
-                    array[i]=mayor;
+
+    public static int[] Descendente(int[] array) {
+        int[] asc = new int[4];
+        for (int i = 0; i < 4; i++) {
+            int mayor = array[i];
+            for (int j = i + 1; j < 4; j++) {
+                int b = array[j];
+                if (mayor <= b) {
+                    mayor = b;
+                    array[j] = array[i];
+                    array[i] = mayor;
                 }
             }
-            asc[i]=mayor;
+            asc[i] = mayor;
         }
         return asc;
     }
-    
-    public static int Resta (int [] num1, int [] num2){
+
+    public static int Resta(int[] num1, int[] num2) {
         int a = Integer.parseInt(ConversionNum(num1));
         int b = Integer.parseInt(ConversionNum(num2));
         int resta;
-        resta = a-b;
+        resta = a - b;
         return resta;
     }
 }
